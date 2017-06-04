f344aa676d15beb9ac595a4d7f16439e64cb8ef9
Henrik Lissner <henrik@lissner.net>
2017-06-04 03:45:10 +0200

    Refactor + cleanup

--- nlinum-hl.el
+++ nlinum-hl.el
@@ -5,29 +5,39 @@
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <henrik@lissner.net>
 ;; Created: Jun 03, 2017
-;; Modified: Jun 03, 2017
+;; Modified: Jun 04, 2017
 ;; Version: 1.0.0
-;; Keywords: nlinum highlight current line
+;; Keywords: nlinum highlight current line faces
 ;; Homepage: https://github.com/hlissner/emacs-nlinum-hl
-;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.4") (nlinum "1.6") (cl-lib "0.5"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
 ;;
-;; Extends nlinum to provide current-line-number highlighting, as well as
-;; various other fixes.
+;; Extends nlinum to provide current-line-number highlighting, plus other fixes.
+;;
+;; It also tries to stave off a nlinum glitch where line numbers disappear
+;; (usually in buffers that have been open a while).
 ;;
 ;;; Installation:
 ;;
 ;; M-x package-install RET nlinum-hl
 ;;
 ;;   (require 'nlinum-hl)
-;;   (add-hook 'nlinum-mode #'nlinum-hl-mode)
+;;   (add-hook 'nlinum-hook #'nlinum-hl-mode))
+;;
+;; Alternatively, use `use-package':
+;;
+;;   (use-package nlinum-hl
+;;     :after nlinum
+;;     :config
+;;     (add-hook 'nlinum-hook #'nlinum-hl-mode))
 ;;
 ;;; Code:
 
 (require 'cl-lib)
+(require 'nlinum)
 
 (defgroup nlinum-hl nil
   "Options for nlinum-hl."
@@ -37,16 +47,17 @@
 (defvar nlinum-hl--line 0)
 
 (defface nlinum-hl-face
-  '(((background dark)  (:inherit linum :foreground "white"))
-    ((background white) (:inherit linum :foreground "black")))
+  '((((background dark))  (:inherit linum :weight bold :foreground "white"))
+    (((background white)) (:inherit linum :weight bold :foreground "black")))
   "Face for the highlighted line number."
   :group 'nlinum-hl)
 
 (defun nlinum-hl-overlay-p (ov)
-  (overlay-get ov 'nlinum))
+  "Return t if OV (an overlay) is an nlinum-hl overlay."
+  (overlay-get ov 'nlinum-hl))
 
 (defun nlinum-hl-line (&rest _)
-  "Highlight the current line number with nlinum."
+  "Highlight the current nlinum line number."
   (while-no-input
     (let* ((pbol (line-beginning-position))
            (peol (1+ pbol))
@@ -76,14 +87,8 @@
               (put-text-property 0 (length str) 'face 'nlinum-hl-face str)
               (setq nlinum-hl--overlay ov))))))))
 
-;; (defun nlinum-hl-unhl-line (&optional pt)
-;;   "Removes the hanging overlay hl-line sometimes leaves behind."
-;;   (ignore-errors
-;;     (dolist (overlay (overlays-at (or pt (point))))
-;;       (when (eq (overlay-get overlay 'face) 'hl-line)
-;;         (delete-overlay overlay)))))
-
 (defun nlinum-hl-flush-window (&optional window)
+  "Redraw nlinum in the current WINDOW, repairing any glitches."
   (let ((window (or window (selected-window)))
         (orig-win (selected-window)))
     (with-selected-window window
@@ -94,22 +99,17 @@
           (nlinum--region (point-min) (max 1 (1- (line-beginning-position))))
           (nlinum--region (min (point-max) (1+ (line-end-position))) (point-max)))))))
 
-;; nlinum has a tendency to lose line numbers over time; a known issue. These
-;; hooks/advisors attempt to stave off these glitches.
 (defun nlinum-hl-flush-all-windows (&rest _)
-  "Fix nlinum margins after major UI changes (like a change of font)."
+  "Flush nlinum in all windows."
   (mapc #'nlinum-hl-flush-window (window-list))
   nil)
 (advice-add #'set-frame-font :after #'nlinum-hl-flush-all-windows)
 
 (defun nlinum-hl-flush (&optional _ norecord)
+  "Advice for `select-window' to flush nlinum before/after switching."
   ;; norecord check is necessary to prevent infinite recursion in
   ;; `select-window'
   (when (not norecord) (nlinum-hl-flush-window)))
-(advice-add #'select-window :before #'nlinum-hl-flush)
-(advice-add #'select-window :after  #'nlinum-hl-flush)
-(add-hook 'focus-in-hook  #'nlinum-hl-flush-all-windows)
-(add-hook 'focus-out-hook #'nlinum-hl-flush-all-windows)
 
 ;;;###autoload
 (define-minor-mode nlinum-hl-mode
@@ -129,6 +129,13 @@
 (eval-after-load "web-mode"
   (lambda ()
     (advice-add #'web-mode-fold-or-unfold :after #'nlinum-hl-flush)))
+
+;; nlinum has a tendency to lose line numbers over time; a known issue. These
+;; hooks/advisors attempt to stave off these glitches.
+(advice-add #'select-window :before #'nlinum-hl-flush)
+(advice-add #'select-window :after  #'nlinum-hl-flush)
+(add-hook 'focus-in-hook  #'nlinum-hl-flush-all-windows)
+(add-hook 'focus-out-hook #'nlinum-hl-flush-all-windows)
 
 (provide 'nlinum-hl)
 ;;; nlinum-hl.el ends here
