c056b76f9f4f7e9c7e4dbe9a17fd2d71ea7c976c
Henrik Lissner <henrik@lissner.net>
2017-06-14 02:46:52 +0200

    Remove unused functions; refactor nlinum-hl-flush-region

--- nlinum-hl.el
+++ nlinum-hl.el
@@ -50,86 +50,14 @@
 
 (defgroup nlinum-hl nil
   "Options for nlinum-hl."
-  :group 'faces) ; FIXME :group
-
-(defcustom nlinum-hl-redraw 'window
-  "Determines what nlinum-hl should do when it encounters a missing line number.
-
-If nil, do nothing about it.
-If 'line', fix only that line number (fastest).
-If 'window', redraw only the visible line numbers in the current window.
-If 'buffer', redraw all line numbers in that buffer.
-If t, redraw nlinum across all buffers (slowest)."
-  :group 'nlinum-hl
-  :type '(choice (const :tag "Current line" 'line)
-                 (const :tag "Visible window" 'window)
-                 (const :tag "Whole buffer" 'buffer)
-                 (const :tag "All windows" t)
-                 (const :tag "Do nothing" nil)))
-
-(defface nlinum-hl-face
-  '((((background dark))  (:inherit linum :weight bold :foreground "white"))
-    (((background white)) (:inherit linum :weight bold :foreground "black")))
-  "Face for the highlighted line number."
-  :group 'nlinum-hl)
-
-(defvar nlinum-hl--overlay nil)
-(defvar nlinum-hl--line "0")
+  :group 'faces)
 
 ;;
-(defun nlinum-hl-overlay-p (ov)
-  "Return t if OV (an overlay) is an nlinum overlay."
-  (overlay-get ov 'nlinum))
-
-(defun nlinum-hl--this-overlay (beg end)
-  "Get the nlinum overlay for the current line."
-  (cl-find-if #'nlinum-hl-overlay-p (overlays-in beg end)))
-
-(defun nlinum-hl-line (&optional force-p)
-  "Highlight the current nlinum line number."
-  (while-no-input
-    (let ((lineno (format-mode-line "%l")))
-      (when (or force-p (not (equal nlinum-hl--line lineno)))
-        (let* ((pbol (line-beginning-position))
-               (peol (min (1+ pbol) (point-max))))
-          (setq nlinum-hl--line lineno)
-          ;; Unhighlight previous highlight
-          (when nlinum-hl--overlay
-            (let ((str (nth 1 (get-text-property 0 'display (overlay-get nlinum-hl--overlay 'before-string)))))
-              (put-text-property 0 (length str) 'face 'linum str)
-              (setq nlinum-hl--overlay nil)))
-          (let ((ov (nlinum-hl--this-overlay pbol peol)))
-            ;; highlight current line number
-            (if ov
-                (nlinum--region pbol peol)
-              ;; Try to deal with evaporated line numbers
-              (unless (eobp)
-                (cond ((eq nlinum-hl-redraw 'line)
-                       (nlinum--region pbol peol))
-                      ((eq nlinum-hl-redraw 'window)
-                       (nlinum-hl-flush-region (window-start) (window-end)))
-                      ((eq nlinum-hl-redraw 'buffer)
-                       (nlinum-hl-flush-window nil t))
-                      ((eq nlinum-hl-redraw t)
-                       (nlinum-hl-flush-all-windows)))))
-            (when (setq ov (nlinum-hl--this-overlay pbol peol))
-              (overlay-put ov 'nlinum-hl t)
-              (let ((str (nth 1 (get-text-property 0 'display (overlay-get ov 'before-string)))))
-                (put-text-property 0 (length str) 'face 'nlinum-hl-face str)
-                (setq nlinum-hl--overlay ov)))))))))
-
-(defun nlinum-hl-flush-region (&optional beg end preserve-hl-p)
-  "Redraw nlinum within the region BEG and END (points).
-
-If PRESERVE-HL-P, then don't affect the current line highlight."
+(defun nlinum-hl-flush-region (&optional beg end)
+  "Redraw nlinum within the region BEG and END (points)."
   (when nlinum-mode
-    (let ((beg (or beg (point-min)))
-          (end (or end (point-max))))
-      (if (not preserve-hl-p)
-          (nlinum--region beg end)
-        ;; done in two steps to leave current line number highlighting alone
-        (nlinum--region beg (max 1 (1- (line-beginning-position))))
-        (nlinum--region (min end (1+ (line-end-position))) end)))))
+    (nlinum--region (or beg (point-min))
+                    (or end (point-max)))))
 
 (defun nlinum-hl-flush-window (&optional window force-p)
   "If line numbers are missing in the current WINDOW, force nlinum to redraw.
@@ -156,9 +84,7 @@ are missing or not."
                 (forward-line 1)
                 (cl-incf lines))))
           (when (or force-p (not (= ovs lines)))
-            (nlinum-hl-flush-region
-             (point-min) (point-max)
-             (or force-p (not (= ovs lines))))))))))
+            (nlinum-hl-flush-region)))))))
 
 (defun nlinum-hl-flush-all-windows (&rest _)
   "Flush nlinum in all windows."
